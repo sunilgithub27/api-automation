c67657b696e9867f325f23b5f4206ac4ca2d8f9b
jkumar29 <sunil.6.kumar@jci.com>
2022-01-15 00:31:28 +0530

    change related to surefire plugin

--- src/main/java/com/test/api/TestGetTenant.java
+++ src/main/java/com/test/api/TestGetTenant.java
@@ -13,6 +13,7 @@ import static io.restassured.RestAssured.*;
 import java.io.IOException;
 
 import org.springframework.test.context.TestExecutionListeners;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -29,23 +30,25 @@ public class TestGetTenant {
 	@BeforeMethod()
 	public void setup() throws IOException {
 		
-		 baseURI =manageConfig.GetTenantUrl()+"/tenants";
+		 baseURI =manageConfig.GetTenantUrl();
 		
-		 jAuthToken="Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJjYjU0ZWRlNy0xOTgwLTRiNzItODc3Mi1hMTVlMzcyNzA2MGUiLCJzdWIiOiJib290c3RyYXAuYWRtaW4iLCJ0dmNVc2VyIjp7InRlbmFudElkIjoiN2EyY2E3NDAtZGZmZi00MzU3LWE2MDctNTZlMWExNTI0ZjA4IiwidXNlckluZm9JZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMSIsImJ1c2luZXNzVW5pdElkIjpudWxsLCJncmFudHMiOlt7InJvbGVJZCI6ImJmMjc3YWNlLTA5ZWEtNGU1ZC1hMjc5LTE0MGIyNzdjMTViMSIsImZlYXR1cmVzIjpbIldFQl9MT0dJTiIsIkNVU1RPTUVSX1BST1ZJU0lPTklORyIsIkRDX1NISVBNRU5UX1NIUF9TT1VSQ0VfQlVfTUFOQUdFIiwiRENfU0hJUE1FTlRfU0hQX1NPVVJDRV9CVV9WSUVXIiwiRklMRV9VUExPQURfQ09ORklHIiwiQlVTSU5FU1NfVU5JVF9QUk9WSVNJT05JTkciLCJEQ19SRUNFSVZJTkdfU0hJUE1FTlRfU09VUkNFX0JVX01BTkFHRSIsIlBBU1NXT1JEX1JFQ09WRVJZIiwiQ09OVEFDVF9NQU5BR0VNRU5UIiwiT1JERVJfQ0FMTEJBQ0tfTUFOQUdFTUVOVCIsIlVTRVJfTUFOQUdFTUVOVCIsIkNPTkZJR1VSQVRJT05fTUFOQUdFTUVOVCIsIlRPS0VOX1JFVk9DQVRJT04iLCJVU0VSX1BBU1NXT1JEX01BTkFHRU1FTlQiLCJEQ19SRUNFSVZJTkdfU0hJUE1FTlRfU09VUkNFX0JVX1ZJRVciLCJTRUNVUklUWV9NSUdSQVRJT05fTUFOQUdFTUVOVCIsIkRFVl9QT1JUQUxfQVBQU19NQU5BR0VNRU5UIiwiR0VUX0ZJTEVfVVBMT0FEX0NPTkZJRyIsIk1BVENIX0NST04iXSwiY29udGV4dExldmVsIjoiQ1VTVE9NRVIiLCJjb250ZXh0cyI6W10sInJvbGVOYW1lIjoiSkNJIEFkbWluIiwiY29udGV4dEJ1c2luZXNzVW5pdElkcyI6W10sImNvbnRleHRUZW5hbnRJZHMiOltdLCJjb250ZXh0U2l0ZUlkcyI6W119XSwiYmVhcmVyVG9rZW5WYWx1ZSI6bnVsbCwiZW1haWwiOiJib290c3RyYXAuYWRtaW5AamNpLmNvbSJ9LCJleHAiOjE2NDI3MDM5NjN9.VRkVC68pGfELEf7VXy0nFyYZ3K9rIqIjbLt5lj_T5JuxXxPY_XV3bv3QutmY5vahUCtIu-kQ5rXa_rzGi9I3XQ";
-				
+		 jAuthToken="Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJmZWYwMWE3ZC1lNzY3LTQ4YmEtYWE3OS1jMWQ5ZGRhZmEwN2MiLCJzdWIiOiJib290c3RyYXAuYWRtaW4iLCJ0dmNVc2VyIjp7InRlbmFudElkIjoiN2EyY2E3NDAtZGZmZi00MzU3LWE2MDctNTZlMWExNTI0ZjA4IiwidXNlckluZm9JZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMSIsImJ1c2luZXNzVW5pdElkIjpudWxsLCJncmFudHMiOlt7InJvbGVJZCI6ImJmMjc3YWNlLTA5ZWEtNGU1ZC1hMjc5LTE0MGIyNzdjMTViMSIsImZlYXR1cmVzIjpbIldFQl9MT0dJTiIsIkNVU1RPTUVSX1BST1ZJU0lPTklORyIsIkRDX1NISVBNRU5UX1NIUF9TT1VSQ0VfQlVfTUFOQUdFIiwiRENfU0hJUE1FTlRfU0hQX1NPVVJDRV9CVV9WSUVXIiwiRklMRV9VUExPQURfQ09ORklHIiwiQlVTSU5FU1NfVU5JVF9QUk9WSVNJT05JTkciLCJEQ19SRUNFSVZJTkdfU0hJUE1FTlRfU09VUkNFX0JVX01BTkFHRSIsIlBBU1NXT1JEX1JFQ09WRVJZIiwiQ09OVEFDVF9NQU5BR0VNRU5UIiwiT1JERVJfQ0FMTEJBQ0tfTUFOQUdFTUVOVCIsIlVTRVJfTUFOQUdFTUVOVCIsIkNPTkZJR1VSQVRJT05fTUFOQUdFTUVOVCIsIlRPS0VOX1JFVk9DQVRJT04iLCJVU0VSX1BBU1NXT1JEX01BTkFHRU1FTlQiLCJEQ19SRUNFSVZJTkdfU0hJUE1FTlRfU09VUkNFX0JVX1ZJRVciLCJTRUNVUklUWV9NSUdSQVRJT05fTUFOQUdFTUVOVCIsIkRFVl9QT1JUQUxfQVBQU19NQU5BR0VNRU5UIiwiR0VUX0ZJTEVfVVBMT0FEX0NPTkZJRyIsIk1BVENIX0NST04iXSwiY29udGV4dExldmVsIjoiQ1VTVE9NRVIiLCJjb250ZXh0cyI6W10sInJvbGVOYW1lIjoiSkNJIEFkbWluIiwiY29udGV4dEJ1c2luZXNzVW5pdElkcyI6W10sImNvbnRleHRUZW5hbnRJZHMiOltdLCJjb250ZXh0U2l0ZUlkcyI6W119XSwiYmVhcmVyVG9rZW5WYWx1ZSI6bnVsbCwiZW1haWwiOiJib290c3RyYXAuYWRtaW5AamNpLmNvbSJ9LCJleHAiOjE2NDMwNTA3ODB9.o6c8AAVaqk3tH8l5v5rswpnyY2F6NYE0UhDYYO_MyQo8OradML1PeFYxPEHcmBSFJGO8kC1Xob0SoAt9_gH_wg";	
 	}
 	
 	
 	@Test()
 	public  void getTenantList() {
 		
-		RestAssured.baseURI= baseURI;
+		
+		String resUrl= baseURI+"/tenants";
+		
+		RestAssured.baseURI= resUrl;
 		
 		RequestSpecification request = RestAssured.given();
 		//request.queryParam("businessUnitId", "b9c53b13-a5d0-4d3f-9095-9c9a22043b99");
 		request.headers("Authorization",jAuthToken);
 		
-		Response response=  request.get(baseURI);
+		Response response=  request.get(resUrl);
 		
 	    ResponseBody  body = response.getBody();
 	
@@ -65,6 +68,38 @@ public class TestGetTenant {
 		
 		System.out.println(numberOfElements);
 		
+		Assert.assertTrue(numberOfElements>=0, "Get Tenant List fails");
+		
+	}
+	
+	@Test(priority=-1)
+	public  void getTenantListByTenantID() {
+		String tenantID= "5e42bb5a-d802-486b-99a2-347531508588";
+		
+		String resUrl= baseURI+"/tenants/"+tenantID;
+		
+		RestAssured.baseURI= resUrl;
+		
+		
+		
+		RequestSpecification request = RestAssured.given();
+	
+		//request.queryParam("businessUnitId", "b9c53b13-a5d0-4d3f-9095-9c9a22043b99");
+		request.headers("Authorization",jAuthToken);
+		
+		Response response=  request.get(resUrl);
+		
+	    ResponseBody  body = response.getBody();
+	
+	    System.out.println(body.prettyPrint());
+		
+		/*
+		 * given().queryParam("businessUnitId", "b9c53b13-a5d0-4d3f-9095-9c9a22043b99").
+		 * and().headers("Authorization",AuthToken). when().get(baseURI).
+		 * then().assertThat().statusCode(200).and().contentType(ContentType.JSON);
+		 */
+		
+		JsonPath bodyasString = body.jsonPath();
 		
 	}
 	
